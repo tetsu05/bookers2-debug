15cf926577c4c6bc04c22dfd300ca006062c9d11
tetsu05 <tetsu051955@gmail.com>
2021-10-04 12:28:43 +0000

    [Middle]

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+  end
   resources :homes
   get 'home/about' => 'homes#about'
-  
+
 end
